af5424a06e0b847ada2b2cd7ed765b3f4be8034d
Radek <radoslaw.biesek@gmail.com>
2019-02-11 21:55:32 +0100

    Create app

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '296077767599-2a19vcad0eipiqccoorq3vsicpvi791r.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'neiNyjM6ZFUcQ2tKtqgmzEhs',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
